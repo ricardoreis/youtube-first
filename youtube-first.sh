593e7f51edc7247ae06415122c7e79e31bdca0a4
Ricardo Reis <ricardo.luis.reis@gmail.com>
2020-04-15 13:57:10 -0300

    Organização do código incicial v.1

--- youtube-first.sh
+++ youtube-first.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
-curl https://www.youtube.com/channel/UCSyG9ph5BJSmPRyzc_eGC4g/ > teste.html
-lista=`grep 'ListItem' teste.html`
+echo
+echo
+echo "ACESSANDO O YOUTUBE"
+echo "-------------------"
+echo "CANAL: youtube.com/channel/UCSyG9ph5BJSmPRyzc_eGC4g/"
+echo
+echo "Verificando último vídeo..."
+echo
+curl https://www.youtube.com/channel/UCSyG9ph5BJSmPRyzc_eGC4g/ > pagina_do_canal
+lista=`grep 'ListItem' pagina_do_canal`
 #echo $lista
 echo $lista | cut -c 22-999999 > texto.txt
 #echo $( cat texto.txt | tr "[:space:]*" "\000" ) | jq '.'
 
 #itemListElement[0].item.itemListElement[0].url
 ultimo_video=`cat texto.txt | jq '.itemListElement[0].item.itemListElement[0].url'`
+echo
+echo "ÚLTIMO VÍDEO"
 echo $ultimo_video
+echo
 
 ultimo_video_comentado=`tail -1 videos_comentados.txt`
+echo "ÚLTIMO VÍDEO COMENTADO"
 echo $ultimo_video_comentado
+echo
 
 if test "$ultimo_video" = "$ultimo_video_comentado"
 then
@@ -21,4 +34,4 @@ fi
 
 echo 1 >> videos_comentados.txt
 
-/home/ricardo/som.sh
+/home/ricardo/youtube-first/som.sh
